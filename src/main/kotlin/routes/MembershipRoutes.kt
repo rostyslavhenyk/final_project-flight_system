5ec91efae67a0b4d4485dce7b14e026bf02cc181
wldk6187 <wldk6187@leeds.ac.uk>
2026-05-08 07:47:54 +0100

    Membership Further Updated

--- src/main/kotlin/routes/MembershipRoutes.kt
+++ src/main/kotlin/routes/MembershipRoutes.kt
@@ -7,10 +7,16 @@ import utils.timed
 
 fun Route.membershipRoutes() {
     get("/membership") { handleMembershipList(call) }
+    get("/membership/benefits") { handleMembershipBenefitsList(call) }
 }
 
 private suspend fun handleMembershipList(call: ApplicationCall) {
     call.timed("T0_membership_list", call.jsMode()) {
         call.renderTemplate("user/membership/index.peb", mapOf("title" to "Membership"))
+    }
+}
+private suspend fun handleMembershipBenefitsList(call: ApplicationCall) {
+    call.timed("T0_membership_benefits_list", call.jsMode()) {
+        call.renderTemplate("user/membership/benefits.peb", mapOf("title" to "Membership"))
     }
 }
